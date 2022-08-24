9b2488d7f7be41ed65570c19513e36c06e2bd0a9
kwonheeyoung <heeyoung8748@gmail.com>
2022-08-24 18:01:04 +0900

    [Feat ] 랜덤 오브젝트 포지션 변경
    일정 시간이 지나면 랜덤한 방 속 오브젝트 하나를 무작위로 선택하여 position을 바꿉니다.
    현재 바뀌는 위치값은 고정이므로 어느 좌표가 얼마나 바뀔지를 랜덤하게 조정할 수 있도록 수정 필요합니다.

--- KeepAnEyeOnArtGallery/Assets/Scripts/Objects/MoveObject.cs
+++ KeepAnEyeOnArtGallery/Assets/Scripts/Objects/MoveObject.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class MoveObject : MonoBehaviour
 {
+    public GameObject FixCheckerPrefab;
+    public int FixCheckerMaxCount = 4;
+    private GameObject[] _fixCheckers;
     private GameObject[] _moveableObjects;
     private int _roomCount = 3;
     int _changeCount = 0;
@@ -12,6 +15,7 @@ public class MoveObject : MonoBehaviour
     void Awake()
     {
         _moveableObjects = new GameObject[_roomCount];
+        _fixCheckers = new GameObject[FixCheckerMaxCount];
         for (int i = 0; i < _roomCount; ++i)
         {
             _moveableObjects[i] = transform.GetChild(i).gameObject;
@@ -28,14 +32,20 @@ public class MoveObject : MonoBehaviour
     {
         int childNum = Random.Range(0, 100) % 3;
         int moveableObjectsInThisRoom = _moveableObjects[childNum].transform.childCount;
-        _changeCount++;
-        //Debug.Log($"{_moveableObjects[childNum].name} 의 {moveableObjectsInThisRoom}개 오브젝트 중에서 랜덤변경. 현재 변경사항 수:{_changeCount}");
         int indexNum = Random.Range(0, moveableObjectsInThisRoom);
+        Debug.Log($"{_moveableObjects[childNum].name} 의 {moveableObjectsInThisRoom}개 오브젝트 중{_moveableObjects[childNum].transform.GetChild(indexNum).gameObject.name}변경.");
         ChangeObject(childNum, indexNum);
     }
 
     private void ChangeObject(int childNum, int indexNum)
     {
-
+        GameObject asdf = _moveableObjects[childNum].transform.GetChild(indexNum).gameObject;
+        Vector3 changedPos = asdf.transform.position;
+        changedPos.y += 3;
+        asdf.transform.position = changedPos;
+        _fixCheckers[_changeCount] = Instantiate(FixCheckerPrefab);
+        _fixCheckers[_changeCount].transform.SetParent(_moveableObjects[childNum].transform, true);
+        _changeCount++;
+        Debug.Log($"현재 변경 사항: {_changeCount}");
     }
 }

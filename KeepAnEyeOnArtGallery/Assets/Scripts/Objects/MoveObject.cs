baabdb4dd07fa3d023db336e1e690956433e2ae9
kwonheeyoung <heeyoung8748@gmail.com>
2022-08-24 16:40:36 +0900

    [Chore] MoveObject 스크립트의 디버그 로그 삭제

--- KeepAnEyeOnArtGallery/Assets/Scripts/Objects/MoveObject.cs
+++ KeepAnEyeOnArtGallery/Assets/Scripts/Objects/MoveObject.cs
@@ -15,7 +15,6 @@ public class MoveObject : MonoBehaviour
         for (int i = 0; i < _roomCount; ++i)
         {
             _moveableObjects[i] = transform.GetChild(i).gameObject;
-            Debug.Log($"{_moveableObjects[i].name} 연결했습니다");
         }
         GameManager.Instance.CanUpdateAnomaly.AddListener(UpdateAnomaly);
     }
@@ -30,7 +29,7 @@ public class MoveObject : MonoBehaviour
         int childNum = Random.Range(0, 100) % 3;
         int moveableObjectsInThisRoom = _moveableObjects[childNum].transform.childCount;
         _changeCount++;
-        Debug.Log($"{_moveableObjects[childNum].name} 의 {moveableObjectsInThisRoom}개 오브젝트 중에서 랜덤으로 변경사항이 생깁니다. 현재 변경사항 수:{_changeCount}");
+        Debug.Log($"{_moveableObjects[childNum].name} 의 {moveableObjectsInThisRoom}개 오브젝트 중에서 랜덤변경. 현재 변경사항 수:{_changeCount}");
     }
     
 }

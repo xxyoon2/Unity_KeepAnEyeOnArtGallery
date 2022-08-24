c7980c0675469f97de9f1f711162a0ed877d088c
xxyoon2 <103083213+xxyoon2@users.noreply.github.com>
2022-08-24 12:12:48 +0900

    [Chore] 디버그로그로 FPS찍기

--- KeepAnEyeOnArtGallery/Assets/Scripts/FpsCheck.cs
+++ KeepAnEyeOnArtGallery/Assets/Scripts/FpsCheck.cs
@@ -5,15 +5,32 @@ using TMPro;
 
 public class FpsCheck : MonoBehaviour
 {
-    private TextMeshProUGUI _ui;
+    [Range(1, 100)] public int fFont_Size;
+    [Range(0, 1)] public float Red, Green, Blue;
+    float deltaTime = 0.0f;
     private void Start()
     {
-        _ui = GetComponent<TextMeshProUGUI>();
+        fFont_Size = fFont_Size == 0 ? 50: fFont_Size;
     }
-    void UpdateText()
+    void Update()
     {
-        float fps = 1.0f / Time.deltaTime;
-        _ui.text = $"Fps: {fps}";
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
     }
 
+    void OnGUI()
+    {
+        int w = Screen.width, h = Screen.height;
+
+        GUIStyle style = new GUIStyle();
+
+        Rect rect = new Rect(0, 0, w, h * 0.02f);
+        style.alignment = TextAnchor.UpperLeft;
+        style.fontSize = h * 2 / fFont_Size;
+        style.normal.textColor = new Color(Red, Green, Blue, 1.0f);
+        float msec = deltaTime * 1000.0f;
+        float fps = 1.0f / deltaTime;
+        //string text = string.Format("{0:0.0} ms ((1:0.} fps)", msec, fps);
+        Debug.Log($"{fps}");
+        //GUI.Label(rect, text, style);
+    }
 }

ba71be8c9019055a45da9a71a3d7f3474d372b1e
kwonheeyoung <heeyoung8748@gmail.com>
2022-08-26 13:55:04 +0900

    [Chore] Aim 아래 상호작용 텍스트 추가

---
+++ KeepAnEyeOnArtGallery/Assets/Scripts/UI/FixNotify.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FixNotify : MonoBehaviour
+{
+    private TextMeshProUGUI _ui;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

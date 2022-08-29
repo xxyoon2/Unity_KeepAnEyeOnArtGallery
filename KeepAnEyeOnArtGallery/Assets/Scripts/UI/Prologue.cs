778f942036d23b807fc554ce7c13b4c1994e928c
kwonheeyoung <heeyoung8748@gmail.com>
2022-08-29 16:55:04 +0900

    [Modify] 배열에 따로 저장하지 않고 인덱스를 통해 출력하기

---
+++ KeepAnEyeOnArtGallery/Assets/Scripts/UI/Prologue.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Prologue : MonoBehaviour
+{
+    private TextMeshProUGUI _ui;
+    private List<DialogueRecord> _dialogueRecords;
+    private int _currentDialogueIndex = 1;
+
+    void Awake()
+    {
+        _ui = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Start()
+    {
+        _dialogueRecords = CSVParser.GetDialogueRecords();
+        _ui.text = _dialogueRecords[_currentDialogueIndex].Text;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ++_currentDialogueIndex;
+            _ui.text = _dialogueRecords[_currentDialogueIndex].Text;
+        }
+    }
+    /*
+      IEnumerator PrologueDialogue()
+     {
+         int i = 0;
+         while (i < _dialogs.Length)
+         {
+             _ui.text = _dialogs[i];
+             if (Input.GetKey(KeyCode.Space))
+             {
+                 yield return new WaitForSeconds(0.1f);
+                 i++;
+             }
+         }
+         _ui.text = "Press spacebar to start game...";
+     }
+     */
+}

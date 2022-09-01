3d0f7ea7902c4e2fa316b403be8ba8ebfce8b860
xxyoon2 <103083213+xxyoon2@users.noreply.github.com>
2022-09-01 12:05:41 +0900

    [Add] 물건 고칠때의 사운드 추가

--- KeepAnEyeOnArtGallery/Assets/Scripts/etc/TitleSound.cs
+++ KeepAnEyeOnArtGallery/Assets/Scripts/etc/TitleSound.cs
@@ -1,22 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class TitleShader : MonoBehaviour
+public class TitleSound : MonoBehaviour
 {
-    public Texture TitleTexture;
-    private CanvasRenderer _canvasRenderer;
     private AudioSource _audioSource;
 
     private void Awake()
     {
-        _canvasRenderer = GetComponent<CanvasRenderer>();
         _audioSource = GetComponent<AudioSource>();
     }
     void Start()
     {
-        _canvasRenderer.SetTexture(TitleTexture);
         _audioSource.Play();
     }
 
